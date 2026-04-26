6a1e35fb13a3672bf4a87a684f718326b781cb66
Sarah Ramoudit <sarahramoudit73@gmail.com>
2026-04-26 09:27:37 -0400

    modified:   public/firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCprvdqLmd-M_d9U2o0ibM_x67C-7U2RYA",
-  authDomain: "info1601-bookapp-610f1.firebaseapp.com",
-  projectId: "info1601-bookapp-610f1",
-  storageBucket: "info1601-bookapp-610f1.firebasestorage.app",
-  messagingSenderId: "179544836983",
-  appId: "1:179544836983:web:336c94bfa90cbb1bb36f72"
-  };
+ apiKey: "AIzaSyCprvdqLmd-M_d9U2o0ibM_x67C-7U2RYA",
+ authDomain: "info1601-bookapp-610f1.firebaseapp.com",
+ projectId: "info1601-bookapp-610f1",
+ storageBucket: "info1601-bookapp-610f1.firebasestorage.app",
+ messagingSenderId: "179544836983",
+ appId: "1:179544836983:web:336c94bfa90cbb1bb36f72"
+};
 
 
   export default firebaseConfig;
